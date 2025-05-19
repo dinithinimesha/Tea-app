c51e91f72746365c338132a161cd864c969cb9ee
Dinithi Nimesha <148965885+dinithinimesha@users.noreply.github.com>
2025-05-19 09:45:53 +0530

    test payment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { StripeProvider } from "@stripe/stripe-react-native";
 export default function RootLayout() {
   return (
     <StripeProvider
-      publishableKey="pk_test_51RBEWGP7K68X3iwBrwXlNeiY1BFrwCoVDmO7lzRFLCD9QIXGryULUcDc5Op9JX7b7IGZq3BHrzggFkWNlm72nZn000FmBeTcXt" // Replace with your Stripe test key
+      publishableKey="pk_test_51RQ8BLGCoDtsBtXJ95TiWp3EMxHpEBrdss7SO1plJCZZeXylXedxylvmobKFZml1gcgwFdInq0DsDF4xlU5Sn9dv00BJIpJsho" // Replace with your Stripe test key
       merchantIdentifier="merchant.identifier" // For Apple Pay
       urlScheme="your-url-scheme" // For 3D Secure / bank redirects
     >
